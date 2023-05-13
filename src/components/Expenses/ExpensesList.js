43ae2659c3a55c93745eb2266cfc2ab4e7e04fab
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-13 19:07:01 +0400

    adding ExpensesChart file

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
   if (props.items.length === 0) {
